e8ef859a7a58d0f18946ddb40a83ff62450b1b16
babuu69 <giorgimiqa4@gmail.com>
2026-08-02 10:52:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-
 const button = document.getElementById("openBook");
 const cover = document.getElementById("cover");
 const book = document.getElementById("book");
 
 button.addEventListener("click", () => {
+
+    createPetalBurst();
+
     cover.style.display = "none";
     book.classList.remove("hidden");
+
+    const music = document.getElementById("bgMusic");
+    if (music) {
+        music.volume = 0.35;
+        music.play();
+    }
 
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
+
 });
 
 function createPetalBurst(){
 
-const petals=["🌸","🌺","🌼","💮","🌷"];
+    const petals=["🌸","🌺","🌼","💮","🌷"];
 
-for(let i=0;i<20;i++){
+    for(let i=0;i<20;i++){
 
-const petal=document.createElement("div");
+        const petal=document.createElement("div");
 
-petal.className="flower";
+        petal.className="flower";
 
-petal.innerHTML=petals[Math.floor(Math.random()*petals.length)];
+        petal.innerHTML=petals[Math.floor(Math.random()*petals.length)];
 
-petal.style.left=Math.random()*window.innerWidth+"px";
+        petal.style.left=Math.random()*window.innerWidth+"px";
 
-petal.style.animationDuration=(3+Math.random()*2)+"s";
+        petal.style.animationDuration=(3+Math.random()*2)+"s";
 
-petal.style.fontSize=(16+Math.random()*18)+"px";
+        petal.style.fontSize=(16+Math.random()*18)+"px";
 
-document.body.appendChild(petal);
+        document.body.appendChild(petal);
 
-setTimeout(()=>{
-petal.remove();
-},5000);
+        setTimeout(()=>{
+            petal.remove();
+        },5000);
 
-}
+    }
 
 }
