5bbf8a84a19a86d72e6e81c14748f6e583043965
babuu69 <giorgimiqa4@gmail.com>
2026-08-02 05:08:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const button = document.getElementById("openBook");
+const cover = document.getElementById("cover");
+const book = document.getElementById("book");
+
+button.addEventListener("click", () => {
+    cover.style.display = "none";
+    book.classList.remove("hidden");
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
